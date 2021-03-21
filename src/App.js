3b328d4e140b4cf2a2c6cccb0369a0977e862a15
zeexoc <zeexoc@outlook.com>
2021-03-21 16:36:15 +0800

    create

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <span>
+            ——长路始焉。
+        </span>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { awaitElement, log, addLocationChangeCallback } from "./utils";
+
+log("React script has successfully started");
+
+// Do required initial work. Gets called every time the URL changes,
+// so that elements can be re-inserted as a user navigates a page with
+// different routes.
+async function main() {
+    // Find <body/>. This can be any element. We wait until
+    // the page has loaded enough for that element to exist.
+    console.log(111)
+    const body = await awaitElement("#header>h2>span");
+    console.log(body)
+    const container = document.createElement("div");
+    body.append(container);
+    ReactDOM.render(<App />, container);
+}
+
+// Call `main()` every time the page URL changes, including on first load.
+addLocationChangeCallback(() => {
+    // Greasemonkey doesn't bubble errors up to the main console,
+    // so we have to catch them manually and log them
+    main().catch(e => {
+        log(e);
+    });
+});
